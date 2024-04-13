cf5933ff91a4cc0c9cfec3ecc7da5590c6b587d5
oguzcoll <oguzcolak2000@gmail.com>
2024-04-13 15:50:50 +0300

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Oguz Colak on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
